e34cc9446bcfca2698d03fb0b3a5131bfbb75708
Otavio Henrique <otaviooliveirapt@hotmail.com>
2024-11-15 20:33:18 -0300

    Modificando o arquivo RetainedProducts

--- CLIENT/import-manager/src/components/view/RetainedProductsView.js
+++ CLIENT/import-manager/src/components/view/RetainedProductsView.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../../styles/styles.css";
+import "../../styles/retainedProducts.css";
 import { Input } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 
