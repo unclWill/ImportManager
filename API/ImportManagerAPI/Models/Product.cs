f2cbd8610678fe0e71f40e539ca6ee34c78a70d2
William Silva <william.silva@viannasempre.com.br>
2024-11-15 17:54:17 -0300

    Criação das models, e do Contexto para a criação da base de dados.

--- API/ImportManagerAPI/Models/Product.cs
+++ API/ImportManagerAPI/Models/Product.cs
@@ -1,18 +1,30 @@
-﻿namespace ImportManagerAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ImportManagerAPI.Models;
+
+[Table("Products")]
 public class Product
 {
-    public long Id {get; set;}
+    [Key] 
+    public long Id { get; set; }
 
+    [MaxLength(50)] 
     public string Name { get; set; }
 
+    [MaxLength(100)] 
     public string Description { get; set; }
 
     public int Quantity { get; set; }
 
+    [Column(TypeName = "decimal(18,2)")] 
     public decimal Price { get; set; }
 
-    public User User { get; set; }
+    public long UserId { get; set; }
+    [ForeignKey("UserId")] 
+    public User Owner { get; set; }
 
-
+    public long CategoryId { get; set; }
+    [ForeignKey("CategoryId")] 
+    public Category Category { get; set; }
 }
