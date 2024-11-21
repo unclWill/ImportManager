b1d5f9703eca40c60b7dadc63bda7b95945f3875
Ricardo Ottoni <ricardo.ottoni@aol.com>
2024-11-20 21:06:54 -0300

    Adicação de máscara em cpf/cnpj - Mascaras.js

--- CLIENT/import-manager/src/utils/Mascaras.js
+++ CLIENT/import-manager/src/utils/Mascaras.js
@@ -1,10 +1,10 @@
 function isValidCPF(cpf) {
-  const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{11}$/;
+  const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
   return cpfRegex.test(cpf);
 }
 
 function isValidCNPJ(cnpj) {
-  const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$|^\d{14}$/;
+  const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/;
   return cnpjRegex.test(cnpj);
 }
 
@@ -15,9 +15,27 @@ function cleanDoc(cpf) {
 function checkDocType(doc) {
   if (isValidCPF(doc)) {
     return "cpf";
-  } else {
+  } else if (isValidCNPJ(doc)) {
     return "cnpj";
   }
+  return "invalid";
 }
 
-export { isValidCNPJ, isValidCPF, cleanDoc, checkDocType };
+function applyCPFMask(cpf) {
+  const cleanValue = cleanDoc(cpf);
+  if (cleanValue.length <= 3) return cleanValue;
+  if (cleanValue.length <= 6) return cleanValue.replace(/^(\d{3})(\d{1,3})/, "$1.$2");
+  if (cleanValue.length <= 9) return cleanValue.replace(/^(\d{3})(\d{3})(\d{1,3})/, "$1.$2.$3");
+  return cleanValue.replace(/^(\d{3})(\d{3})(\d{3})(\d{1,2})/, "$1.$2.$3-$4");
+}
+
+function applyCNPJMask(cnpj) {
+  const cleanValue = cleanDoc(cnpj).slice(0, 14);
+  if (cleanValue.length <= 2) return cleanValue;
+  if (cleanValue.length <= 5) return cleanValue.replace(/^(\d{2})(\d{1,3})/, "$1.$2");
+  if (cleanValue.length <= 8) return cleanValue.replace(/^(\d{2})(\d{3})(\d{1,3})/, "$1.$2.$3");
+  if (cleanValue.length <= 12) return cleanValue.replace(/^(\d{2})(\d{3})(\d{3})(\d{1,4})/, "$1.$2.$3/$4");
+  return cleanValue.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{1,2})/, "$1.$2.$3/$4-$5");
+}
+
+export { isValidCNPJ, isValidCPF, cleanDoc, checkDocType, applyCPFMask, applyCNPJMask };
